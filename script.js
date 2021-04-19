e52ce8105b1223dc82c05af42baa300aeeb25449
Sergiy Chumachenko <chumachenko.sergiy@gmail.com>
2021-04-19 21:19:54 +0300

    'highscore' functionality has been implemented

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 "use strict";
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1,
-  score = 20;
+  score = 20,
+  highScore = 0;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -12,6 +13,10 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".message").textContent = "🎉 Correct Number!";
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector(".highscore").textContent = highScore;
+    }
   } else {
     score--;
     if (score > 0) {
